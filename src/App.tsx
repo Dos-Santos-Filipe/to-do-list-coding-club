d04a895129715b45e8496c67dc2f7d17180b91b0
Filipe Dos Santos <filipesantos.psic@gmail.com>
2025-03-16 19:26:26 -0300

    [WIP]: Add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
 
-
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+  const addTask = () => {
+    setTasks([...tasks, inputValue]);
+    setInputValue("");
+  };
 
   return (
     <div className="main">
       <h1 className="title">To-do List</h1>
       <div className="list-container"></div>
       <div className="form">
-        <input type="text" placeholder="Add a task" />
-        <button type="submit">Add</button>
+        <input
+          type="text"
+          placeholder="New task"
+          onChange={handleInputChange}
+          value={inputValue}
+        />
+        <button type="submit" onClick={addTask}>
+          Add
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
