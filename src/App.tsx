b902739f4b41f7115660546f7c7e2ae85b907d27
Filipe Dos Santos <filipesantos.psic@gmail.com>
2025-05-06 20:34:30 -0300

    Fix PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+
+// API begins ...
+const apiUrl = "http://localhost:3200/api";
+
+type Task = {
+  id?: number;
+  title: string;
+  status: string;
+};
+
+const apiGetTasks = async () => {
+  const response = await fetch(`${apiUrl}/tasks`, { method: "GET" });
+  const res = await response.json();
+  return res.data;
+};
+
+const apiSaveTask = async (task: Task) => {
+  const response = await fetch(`${apiUrl}/task`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
+  });
+  const data: Task = await response.json();
+  return data;
+};
+
+const apiDeleteTask = async (taskId: { id: number }) => {
+  await fetch(`${apiUrl}/task/${taskId.id}`, { method: "DELETE" });
+};
+
+// API ends
+
 function App() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [inputValue, setInputValue] = useState("");
   const [editIndex, setEditIndex] = useState<number>(-1);
   const [editValue, setEditValue] = useState("");
@@ -10,39 +42,72 @@ function App() {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
-  const addTask = () => {
+  const addTask = async () => {
     if (inputValue.trim() === "") return;
-    setTasks([...tasks, inputValue]);
+
+    const newTask: Task = {
+      id: Date.now(),
+      title: inputValue,
+      status: "incomplete"
+    };
+
+    const savedTask = await apiSaveTask(newTask);
+    setTasks([...tasks, savedTask]);
     setInputValue("");
   };
 
-  const editTask = (index: number) => {
-    setEditIndex(index);
-    setEditValue(tasks[index]);
+  const editTask = (id: number) => {
+    const task = tasks.find((task) => task.id === id);
+    if (!task) return;
+    setEditIndex(id);
+    setEditValue(task.title);
   };
 
-  const saveEditTask = () => {
+  const saveEditTask = async () => {
     if (editValue.trim() === "") return;
-    const tempTasks = [...tasks];
-    tempTasks[editIndex] = editValue;
-    setTasks(tempTasks);
+
+    const editedTask = tasks.find((task) => task.id === editIndex);
+    if (!editedTask) return;
+
+    const update = { ...editedTask, title: editValue };
+
+    const res = await fetch(`${apiUrl}/task/${update.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(update),
+    });
+
+    const updatedTasks = await res.json();
+    setTasks(tasks.map(task => task.id === updatedTasks.id ? updatedTasks : task));
     setEditIndex(-1);
     setEditValue("");
   };
 
-  const deleteTask = (index: number) => {
-    const tempTasks = [...tasks];
-    tempTasks.splice(index, 1);
-    setTasks(tempTasks);
+  const deleteTask = async (id: number) => {
+    try{
+      await apiDeleteTask({ id });
+      
+      setTasks(tasks.filter((task) => task.id !== id));
+    } catch (error) {
+      console.error("Error deleting task: ", error); 
+    }
   };
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await apiGetTasks();
+      setTasks(res);
+    };
+    fetchTasks();
+  }, []);
 
   return (
     <div className="main">
       <h1 className="title">To-do List</h1>
       <div className="list-container">
-        {tasks.map((task, index) => (
-          <div className="list-item" key={index}>
-            {editIndex === index ? (
+        {tasks.length === 0 ? <p>No Tasks yet</p> : tasks.map((task) => (
+          <div className="list-item" key={task.id}>
+            {editIndex === task.id ? (
               <div className="edit-container">
                 <input
                   type="text"
@@ -54,14 +119,14 @@ function App() {
               </div>
             ) : (
               <div className="task-container">
-                <span className="task">{task}</span>
+                <span className="task">{task.title}</span>
                 <div className="btns-container">
-                  <button className="edit-btn" onClick={() => editTask(index)}>
+                  <button className="edit-btn" onClick={() => editTask(task.id!)}>
                     Edit
                   </button>
                   <button
                     className="delete-btn"
-                    onClick={() => deleteTask(index)}
+                    onClick={() => deleteTask(task.id!)}
                   >
                     Delete
                   </button>
